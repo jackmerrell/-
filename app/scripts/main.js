7ee9e81cc65b17b06e90399e7b472b0937cbd6b3
Jack Merrell <jackmerrell@gmail.com>
2020-04-20 22:43:33 +0200

    up to speed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,11 @@
-
-document.querySelector('a-entity').addEventListener('click', function (evt) {
+document.querySelector('a-entity').addEventListener('mouseenter', function (evt) {
   var currentVideo = evt.detail.intersection.object.el;
   var currentVideoID = currentVideo.id+'Video';
-  playVideo(currentVideoID)
+  document.getElementById(currentVideoID).play();
+  console.log(currentVideoID+' is playing');
+
+  this.addEventListener('mouseleave', function (evt) {
+    document.getElementById(currentVideoID).pause();
+    console.log(currentVideoID+' is paused');
+    });
 });
-
-
-function playVideo(currentVideoID) {
-
-  var video = document.getElementById(currentVideoID);
-  console.log(video);
-  video.play();
-  video.muted = !video.muted;
-
-}
