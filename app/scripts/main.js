6851ef6a65ad2bc05d05443fb87386e47cce6808
Jack Merrell <jackmerrell@gmail.com>
2020-04-20 23:31:52 +0200

    reduced js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,10 @@
 document.querySelector('a-entity').addEventListener('mouseenter', function (evt) {
-  var currentVideo = evt.detail.intersection.object.el;
-  var currentVideoID = currentVideo.id+'Video';
-  document.getElementById(currentVideoID).play();
-  console.log(currentVideoID+' is playing');
+  var currentVideoFrame = evt.detail.intersection.object.el;
+  var currentVideoID = currentVideoFrame.id+'Video';
+  var video = document.getElementById(currentVideoID);
+  video.play();
 
   this.addEventListener('mouseleave', function (evt) {
-    document.getElementById(currentVideoID).pause();
-    console.log(currentVideoID+' is paused');
+    video.pause();
     });
 });
