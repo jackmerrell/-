2e1ddcbf7c9c938a639e4c7314b11e8e4d8cab92
NotJack <test@test.com>
2020-04-22 00:27:08 +0200

    Bind multiple click events to the initialization button

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,27 +1,15 @@
 const entities = document.querySelectorAll('a-video');
+const button = document.getElementById('test-button');
 
 for (let i = 0, total = entities.length; i < total; i++) {
   const entity = entities[i];
   const video = document.getElementById(`${entity.id}Video`);
   
-  if (i === 0) {
-    const button = document.getElementById('test-button');
-
-    /*
-     * We want to play the video (line #17) first by reacting to a click/touch
-     * event, but at the same time we don't want to triggr the playback
-     * at this point, so we are immediately pausing the video (line #18).
-     * 
-     * Now, the browser knows that we are fine with playing the video, which
-     * solves the promise error you were seeing on desktop.
-     */
-
-    button.onclick = () => {
-      video.play().then(() => {
-        video.pause();
-      });
-    };
-  }
+  button.addEventListener('click', () => {
+    video.play().then(() => {
+      video.pause();
+    });
+  });
 
   entity.addEventListener('mouseenter', () => {
     video.play();
