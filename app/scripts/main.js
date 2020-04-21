8399a140138e2b5f5285e0dc7494d2048e1f8bba
jackmez <jack@forpeople.com>
2020-04-22 01:53:30 +0200

    added welcome screen

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,16 @@
 const entities = document.querySelectorAll('a-video');
 const button = document.getElementById('test-button');
+const loadingScreen = document.getElementById('loadingScreen');
 
 for (let i = 0, total = entities.length; i < total; i++) {
   const entity = entities[i];
   const video = document.getElementById(`${entity.id}Video`);
-  
+
   button.addEventListener('click', () => {
     video.play().then(() => {
       video.pause();
     });
+    loadingScreen.classList.add('hidden');
   });
 
   entity.addEventListener('mouseenter', () => {
