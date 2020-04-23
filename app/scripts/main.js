13cb78c9a75b0ce19df785b0ea556efff7288cc6
Jack Merrell <jackmerrell@gmail.com>
2020-04-23 20:01:35 +0200

    updated landing message

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,10 @@
 const entities = document.querySelectorAll('a-video');
-const button = document.getElementById('test-button');
+const button = document.getElementById('start-button');
 const loadingScreen = document.getElementById('loadingScreen');
+const videosContainer = document.getElementById('videosContainer');
+const loadingCamera = document.getElementById('videoCamera');
+const videoCamera = document.getElementById('videoCamera');
+const cursor = document.getElementById('cursor');
 
 for (let i = 0, total = entities.length; i < total; i++) {
   const entity = entities[i];
@@ -11,6 +15,7 @@ for (let i = 0, total = entities.length; i < total; i++) {
       video.pause();
     });
     loadingScreen.classList.add('hidden');
+    changeCamera();
   });
 
   entity.addEventListener('mouseenter', () => {
@@ -20,4 +25,15 @@ for (let i = 0, total = entities.length; i < total; i++) {
   entity.addEventListener('mouseleave', () => {
     video.pause();
   });
+}
+
+
+function changeCamera(){
+  loadingCamera.setAttribute('camera', 'active', false);
+  videoCamera.setAttribute('camera', 'active', true);
+
+  loadingCamera.setAttribute('animation', 'property:position; from:0 100 250; to:0 0 0; duration:2000;');
+  videosContainer.removeAttribute('animation');
+  videosContainer.setAttribute('rotation','0 0 0');
+  cursor.setAttribute('cursor','fuse: true;');
 }
